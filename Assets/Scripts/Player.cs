e841afb4146ed62d5c6163e758a3ec990be5e6fa
msmolinski <msmolinski.2001@wp.pl>
2023-04-08 15:40:53 +0200

    Moving player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : SingletoneMonobehaviour<Player>
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
